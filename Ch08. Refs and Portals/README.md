593dcea644bee4cc3037b6e46aecf2dea27c2fc6
Julia Park <ya.park@mail.utoronto.ca>
2026-04-02 16:14:12 -0300

    manage user input with state

--- Ch08. Refs and Portals/README.md
+++ Ch08. Refs and Portals/README.md
@@ -1,1 +1,8 @@
+Refs
+simplified direct access to DOM
+manage values that shouldn't be states
+epxose api to othre components
 
+
+Portals
+detach where elements are rendered in the dom vs where it is in t he jsx code
--- Ch08. Refs and Portals/src/components/Player.jsx
+++ Ch08. Refs and Portals/src/components/Player.jsx
@@ -1,10 +1,31 @@
+/* Task 1: Let the input name be reflected in the h2 element */
+
+import { useState } from "react";
+
 export default function Player() {
+
+  /* Problem: Whenever there is a key stroke, h2 element gets
+  updated in real-time even though change has not been submitted yet.
+  There are ways to get around, but there already is enough code. */
+
+  const [enteredName, setEnteredName] = useState(null);
+  const [nameSubmitted, setNameSubmitted] = useState(false);
+
+  function handleNameChange(event) {
+    setNameSubmitted(false);
+    setEnteredName(event.target.value);
+  }
+
+  function handleSetName() {
+    setNameSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {nameSubmitted ? enteredName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={enteredName}/>
+        <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
   );
