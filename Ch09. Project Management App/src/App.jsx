e288c6ae4efbcfbfa2ed4e5db00a16718e943838
Julia Park <ya.park@mail.utoronto.ca>
2026-04-09 18:25:10 -0300

    core components

--- Ch09. Project Management App/src/App.jsx
+++ Ch09. Project Management App/src/App.jsx
@@ -1,8 +1,14 @@
+import NewProject from "./components/NewProject";
+import NoProject from "./components/NoProject";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar />
+      <NoProject />
+      {/* <NewProject /> */}
+    </main>
   );
 }
 
