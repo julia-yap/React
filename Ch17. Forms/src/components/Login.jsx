7d72a5c58aee9b979a0ce78fbf6e77b43750f162
Julia Park <ya.park@mail.utoronto.ca>
2026-06-29 14:59:46 -0300

    strategies to get form inputs

--- Ch17. Forms/src/components/Login.jsx
+++ Ch17. Forms/src/components/Login.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
 
+  // Handling form data
+  // Approach 1. Use states for email and password, and attach
+  // a handler on form
+  // const [enteredValues, setEnteredValues] = useState({
+  //   email: "",
+  //   password: "",
+  // });
+  // function handleInputChange(identifier, value) {
+  //   setEnteredValues((prev) => ({
+  //     ...prev,
+  //     [identifier]: value,
+  //   }));
+
+  // Approach 2. Use refs. (Downside: not recommended to use)
+  const email = useRef();
+  const password = useRef();
+  
   function handleSubmit(event) {
-    // Prevents the default browser behaviour
     event.preventDefault();
-  }
 
-  function handleInputChange(identifier, value) {
-    setEnteredValues((prev) => ({
-      ...prev,
-      [identifier]: value,
-    }));
-  }
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
 
-  // By default, the browser sends a HTTP request with form data
-  // whenever a button inside the form is clicked. Thereafter, the
-  // whole component will get reloaded. To prevent this, set type="button"
-  // on the button element, or set onSubmit on the form element instead
-  // of onClick on the button.
+    console.log(enteredEmail, enteredPassword);
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -31,36 +35,18 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(event) => {
-              handleInputChange("email", event.target.value);
-            }}
-            value={enteredValues.email}
-          />
+          <input id="email" type="email" name="email" ref={email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-            value={enteredValues.password}
-          />
+          <input id="password" type="password" name="password" ref={password}/>
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button type="button" className="button">
-          Login
-        </button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
