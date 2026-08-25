6ccf8af7ba13ac252bb41a823b59a51db596d3a8
Julia Park <ya.park@mail.utoronto.ca>
2026-08-25 15:42:40 -0300

    using an action creator thunk

--- Ch20. Advanced Redux/src/App.js
+++ Ch20. Advanced Redux/src/App.js
@@ -4,7 +4,7 @@ import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 import { useSelector, useDispatch } from "react-redux";
 import { Fragment, useEffect } from "react";
-import { uiActions } from "./store/ui";
+import { sendCartData } from "./store/cart";
 
 let isInitialLoad = true;
 
@@ -16,54 +16,13 @@ function App() {
   // Once Redux updates the store, then write to Firebase => useEffect()
   const cart = useSelector((state) => state.cart);
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.setNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        }),
-      );
-
-      const res = await fetch(
-        "https://react-e2f9b-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        },
-      );
-
-      if (!res.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      // Show notification at the start and finish of the request.
-      // Manage this state with Redux.
-      dispatch(
-        uiActions.setNotification({
-          status: "success",
-          title: "Sent!",
-          message: "Sent cart data successfully!",
-        }),
-      );
-    };
-
     // Don't send the initial state to db (unnecessary notification rendering)
-      if (isInitialLoad) {
-        isInitialLoad = false;
-        return;
-      }
+    if (isInitialLoad) {
+      isInitialLoad = false;
+      return;
+    }
 
-    // Handles any types of error that happens anywhere
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.setNotification({
-          status: "error",
-          title: "Error!",
-          message: "Failed to send cart data.",
-        }),
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   // Add dispatch() to dependeny. React Redux will ensure the function never
   // changes and there will be no call raised by dispatch.
--- Ch20. Advanced Redux/src/store/cart.js
+++ Ch20. Advanced Redux/src/store/cart.js
@@ -1,44 +1,94 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui";
 
 const initialState = {
-    items: {}, 
-    totalQauntity: 0,
-}
+  items: {},
+  totalQauntity: 0,
+};
 
 const cartSlice = createSlice({
-    name: "cart",
-    initialState, 
-    reducers: {
-        addItem(state, action) {
-            const id = action.payload.id;
-            const title = action.payload.title;
-            const price = action.payload.price;
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const id = action.payload.id;
+      const title = action.payload.title;
+      const price = action.payload.price;
 
-            state.totalQauntity ++;
+      state.totalQauntity++;
 
-            const itemExists = id in state.items;
+      const itemExists = id in state.items;
 
-            if (itemExists) {
-                state.items[id].quantity ++;
-            } else {
-                state.items[id] = {
-                    id,
-                    title,
-                    price, 
-                    quantity: 1
-                }
-            }
-        }, 
-        removeItem(state, action) {
-            const id = action.payload.id;
-            const itemExists = id in state.items;
-            if (itemExists && state.items[id].quantity > 0) {
-                state.items[id].quantity --;
-                state.totalQauntity --;
-            }
-        }, 
+      if (itemExists) {
+        state.items[id].quantity++;
+      } else {
+        state.items[id] = {
+          id,
+          title,
+          price,
+          quantity: 1,
+        };
+      }
+    },
+    removeItem(state, action) {
+      const id = action.payload.id;
+      const itemExists = id in state.items;
+      if (itemExists && state.items[id].quantity > 0) {
+        state.items[id].quantity--;
+        state.totalQauntity--;
+      }
+    },
+  },
+});
+
+// Redux toolkit supports action creators returning a function
+// When it sees that there is a dispatching of function instead of
+// actions, then it will execute the function for you and give the dispatch
+// argument automatically, so that we can dispatch again.
+export const sendCartData = (cart) => {
+  // With Thunk, return a function
+  return async (dispatch) => {
+    dispatch(
+      uiActions.setNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      }),
+    );
+
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://react-e2f9b-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        },
+      );
+
+      if (!res.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.setNotification({
+          status: "success",
+          title: "Sent!",
+          message: "Sent cart data successfully!",
+        }),
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.setNotification({
+          status: "error",
+          title: "Error!",
+          message: "Failed to send cart data.",
+        }),
+      );
     }
-})
-
+  };
+};
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
